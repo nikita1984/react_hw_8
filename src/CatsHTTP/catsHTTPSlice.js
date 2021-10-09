4f6b83746c10e6d04a493409ad71786f586af02a
nikita1984 <52231270+nikita1984@users.noreply.github.com>
2021-10-09 20:18:48 +0300

    create relative path for catWaiting.jpg

--- src/CatsHTTP/catsHTTPSlice.js
+++ src/CatsHTTP/catsHTTPSlice.js
@@ -1,6 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-export const defaultData = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8NQzj2JqAe6Gtd65a7iTvm0-wKb2NlluwKA&usqp=CAU";
+// export const defaultData = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8NQzj2JqAe6Gtd65a7iTvm0-wKb2NlluwKA&usqp=CAU";
+export const defaultData = "images/catWaiting.jpg";
 
 export const catsHTTPSlice = createSlice({
   name: "catsHTTP",
